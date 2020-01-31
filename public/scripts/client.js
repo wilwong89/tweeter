95fa69b1cf7b31f7fa72ada5ae44945a2fbb3028
Wilson Wong <wilwong89@gmail.com>
2020-01-31 00:16:34 +0000

    Linting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,8 +21,8 @@ const data = [
     },
     "created_at": 1461113959088
   }
-]
-const url = "/tweets"
+];
+const url = "/tweets";
 const currentUser = {
   "user": {
     "name": "Wilson Wong",
@@ -32,9 +32,9 @@ const currentUser = {
     "text": ""
   },
   "created_at": ""
-}
+};
 
-const tweetValidation = function (textInput) {
+const tweetValidation = function(textInput) {
   // Input validation, returns true or false given a text string.
   if (!textInput) {
     $('.error-container').text("There is no text in tweet.");
@@ -44,54 +44,54 @@ const tweetValidation = function (textInput) {
     $('.error-container').text("This tweet is longer than 140 characters.");
     $('.error-container').slideDown();
     return false;
-  };
+  }
 
   $('.error-container').text("");
   $('.error-container').slideUp();
   return true;
-}
+};
 
-const addSelfTweet = function (inputText) {
+const addSelfTweet = function(inputText) {
   // Creates a psuedo tweet object given an input string
   currentUser.content.text = inputText;
   currentUser.created_at = new Date();
 
   return currentUser;
-}
+};
 
-const loadTweets = function (selfTweet) {
+const loadTweets = function(selfTweet) {
   // First empties all tweets in UI, then repopulates US with tweet elements
   // If Given a tweet object, adds it to the bottoms of tweets to post (for adding own tweet)
-  $(".tweet-container").empty()
+  $(".tweet-container").empty();
   $.ajax({
     type: "GET",
     url: url,
     success: function(tweetsArray) {
       if (selfTweet) {
         tweetsArray.pop();
-        tweetsArray.push(selfTweet)
+        tweetsArray.push(selfTweet);
       }
       renderTweets(tweetsArray);
     }
   });
-}
+};
 
 const renderTweets = function(tweets, currentTweet) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
   for (let tweet of tweets) {
-    $(".tweet-container").prepend(createTweetElement(tweet))
+    $(".tweet-container").prepend(createTweetElement(tweet));
   }
   
-}
+};
 
 const escape =  function(str) {
   // Text filter for templates
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
 const createTweetElement = function(tweet) {
   // Given tweet data object, returns HTML text element to be added to UI.
@@ -108,20 +108,20 @@ const createTweetElement = function(tweet) {
         <textarea maxlength="" id="tweet-template-text" disabled class="tweet-input fwn" name="text">${escape(tweetData.content.text)}</textarea>
         <span class="post-date">${new Date(tweetData.created_at)}</span>
       </div>
-    </section>`
+    </section>`;
   
   return stringTemplate;
-}
+};
 
 // Javascript to run when document is loaded.
-$(document).ready(function(){
+$(document).ready(function() {
   loadTweets();
 
   // Section below runs when form is submitted.
-  $("form").on('submit', function (event) {
+  $("form").on('submit', function(event) {
     event.preventDefault();
-    var form = $(this);
-    var textInput = $(".actual-input").val();
+    let form = $(this);
+    let textInput = $(".actual-input").val();
 
     if (!tweetValidation(textInput)) return;
 
@@ -129,16 +129,16 @@ $(document).ready(function(){
       type: "POST",
       url: url,
       data: form.serialize(),
-      success: function(data) {
-        console.log("success")
+      success: function() {
+        console.log("Tweet success");
         loadTweets(addSelfTweet(textInput));
         $(".counter").text("140");
-        $(".new-tweet").slideUp(400)
+        $(".new-tweet").slideUp(400);
       },
       error: function(err) {
-        console.log("error",)
+        console.log("error", err);
       }
     });
-  })
+  });
   
-})
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-  var maxLength = 140;
-  $(".counter").text(maxLength)
+  let maxLength = 140;
+  $(".counter").text(maxLength);
 
   // Dom manipulation to toggle view of tweet input
-  $(".nav-right").click(function(event){
-    $(".new-tweet").slideToggle(400)
-    $(".error-container").slideUp(400)
-    $(".actual-input").focus()
-  })
+  $(".nav-right").click(function() {
+    $(".new-tweet").slideToggle(400);
+    $(".error-container").slideUp(400);
+    $(".actual-input").focus();
+  });
   
-  // Dom manipulation to controll counter css 
-  $(".tweet-input").on('input', function calcLength() {
+  // Dom manipulation to controll counter css
+  $(".tweet-input").on('input', function {
     $(".counter").text(maxLength - $(".tweet-input").val().length);
 
     if ($(".tweet-input").val().length > maxLength) {
-      $(".counter").css("color","red")
+      $(".counter").css("color","red");
     } else {
-      $(".counter").css("color","black")
+      $(".counter").css("color","black");
     }
-  })
+  });
 });
